ffa4a08dd0a68c88be796903c4c6d3f79643b962
Jonatan Cardoso Saraiva <jonatan.cardoso@hotmail.com.br>
2015-10-28 02:52:11 -0200

    Internalização
    Internalização

---
+++ src/br/com/fiap/nac/bean/LocaleBean.java
@@ -0,0 +1,32 @@
+package br.com.fiap.nac.bean;
+
+import java.util.Locale;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleBean {
+
+	private Locale currentLocale = new Locale("pt", "BR");
+
+	public void englishLocale() {
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		currentLocale = Locale.US;
+		viewRoot.setLocale(currentLocale);
+	}
+
+	public void portugueseLocale() {
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		currentLocale = new Locale("pt", "BR");
+		viewRoot.setLocale(currentLocale);
+	}
+
+	public Locale getCurrentLocale() {
+		return currentLocale;
+	}
+
+}
